a1b2ccc529c645453cccbab2df6f82acc486dd3e
igorsaulo <igorsaulo01@gmail.com>
2023-06-04 11:17:31 -0300

    remove middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,10 +16,5 @@ import { PrismaModule } from './prisma/prisma.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware)
-      .forRoutes('user');
-  }
-}
+export class AppModule {}
+
